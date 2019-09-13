e37f5226952647392635346102ec0e93d7cb8fed
Brayan Diaz <brayanestive18@gmail.com>
2019-09-13 16:48:13 -0500

    Ajuste en el ancho de las columnas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import TodoForm from "./components/TodoForm";
 function App() {
   const todoItems = todos.map((todo, i) => {
     return (
-      <div className="col-sm-4" key={i}>
+      <div className="col-xl-3 col-lg-4 col-md-6 h-100 w-auto" key={i}>
         <div className="card border-success mt-4">
           <div className="card-header">
             <h3 className="text-dark">{todo.title}</h3>
@@ -18,7 +18,7 @@ function App() {
             <p className="rounded bg-primary text-white">{todo.responsible}</p>
           </div>
           <div className="card-footer">
-            <span className="badge badge-pill badge-danger p-2">
+            <span className="badge badge-pill badge-danger p-2" style={{width: 80}}>
               {todo.priority}
             </span>
           </div>
@@ -32,10 +32,10 @@ function App() {
       <h1 className="App-title mt-2">Bienvenido Brayan</h1>
       <div>
         <div className="row no-gutters">
-          <div className="col-auto">
+          <div className="col-sm-3 col-md-5 col-lg-3 col-xl-3">
             <TodoForm></TodoForm>
           </div>
-          <div className="col-auto">
+          <div className="col-sm-9 col-md-7 col-lg-9 col-xl-9">
             <div className="container">
               <div className="row">{todoItems}</div>
             </div>
